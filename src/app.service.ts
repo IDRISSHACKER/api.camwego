9e9d5125aa3266ba183397ede87eac95ce75bacc
IDR6IX DUVAL <idrissleutch@cinaf.tv>
2023-04-11 05:02:14 +0100

    go to v 1.5 v2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Camwego API! V1.4';
+    return 'Camwego API! V1.5';
   }
 
   getAuthor(): string {
