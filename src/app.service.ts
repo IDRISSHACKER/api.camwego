39925130d99a4217428cd88c199edbc1b1d6a3e9
IDR6IX DUVAL <idrissleutch@cinaf.tv>
2023-05-18 22:41:43 +0100

    Integritie global 0002

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Camwego API! V1.9';
+    return 'Camwego API! V2-beta';
   }
 
   getAuthor(): string {
